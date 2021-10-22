bcf7773000215394c99daba9dfce77bdba121cd0
protoko31 <93002207+protoko31@users.noreply.github.com>
2021-10-22 20:22:28 +0300

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,14 @@
+const config = { 
+//------------Connections-----------//
+    Client_Token: 'TOKEN',
+    MongoDB_ConnectURL: 'MONGO URL',
+    //------------BotSettings-----------//
+    Prefix: '.',
+    BotOwners: ["OWNER İD"],
+    Custom_Status_Text: 'ASPENDOS ❤️ PUSHA',
+    Custom_Status_Type: 'PLAYING', // => PLAYING / WATCHING / LISTENING
+    Custom_Status: 'dnd', // => dnd / idle / online / invisible
+   
+};
+  
+module.exports = config;
